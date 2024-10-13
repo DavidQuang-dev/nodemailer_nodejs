30c1a71c2a195266eae60f8b3ad0ba541c4afb2e
David Quang <tranngvietquang04@gmail.com>
2024-10-14 03:21:38 +0700

    update function sendMail

--- app.js
+++ app.js
@@ -1,41 +1,25 @@
-const {google} = require("googleapis");
 const nodemailer = require("nodemailer");
-require("dotenv").config();
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-const REFESH_TOKEN = process.env.REFESH_TOKEN;
-
-const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
-oauth2Client.setCredentials({ refresh_token: REFESH_TOKEN });
-
-const sendMail = async () => {
-    try {
-        const accessToken = await oauth2Client.getAccessToken();
-        const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth:{
-            type: "OAuth2",
-            user: "bussiness@gmail.com",
-            clientId: CLIENT_ID,
-            clientSecret: CLIENT_SECRET,
-            refreshToken: REFESH_TOKEN,
-            accessToken: accessToken
-            }
-        })
-        const mailOptions = {
-            from: "bussiness@gmail.com",
-            to: "user@gmail.com",
-            subject: "Hello",
-            text: "Hello World",
-            html: "<h1>Hello World</h1>"
-        }
-        const result = await transporter.sendMail(mailOptions);
-        console.log(result);
-    } catch (error) {
-        console.log(error);
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    secure: true,
+    port: 465,
+    auth: {
+        user: "bussiness@gmail.com",
+        pass: ""
     }
+})
+
+
+function sendMail(to, subject, msg) {
+    transporter.sendMail({
+        from: "bussiness@gmail.com",
+        to: to,
+        subject: subject,
+        text: msg,
+        html: "<h1>Hello World</h1>"
+    })
 }
 
-sendMail();
+sendMail("user@gmail.com", "Hello", "Hello World");
+
